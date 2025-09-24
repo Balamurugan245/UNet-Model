693b7fd99f0524860e164552530f9a1610f9280f
Balamurugan245 <144324318+Balamurugan245@users.noreply.github.com>
2025-09-24 20:04:26 +0530

    Update loss_fuction.py

--- loss_fuction.py
+++ loss_fuction.py
@@ -1,11 +1,14 @@
 import torch as t
 import torch.nn as nn
+import torch.nn.functional as F
 
 class IoULoss(nn.Module):
     def __init__(self):
         super().__init__()
  
     def forward(self, y_pred, y_true):
+        y_true = torch.where(y_true <= 0, torch.ones_like(y_pred, device=device), torch.zeros_like(y_pred, device=device))
+        y_pred = F.sigmoid(y_pred)
         intersection = (y_pred * y_true).sum()
         union = (y_pred + y_true).sum() - intersection
         iou = (intersection + 1e-5) / (t.abs(union) + 1e-5)
